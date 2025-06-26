75bb8f55df78a8856669baeb02c22670d1dc0d28
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-06-26 18:27:58 +0300

    Add functionality to dislay popular books

--- src/index.js
+++ src/index.js
@@ -27,18 +27,18 @@ function displaySearchResults(userInput) {
         console.log(resultsContainer);
         resultsContainer.innerHTML = ""; // Clear previous results
         // Limit to 10 results
-        const books = data.docs.slice(0, 10);
+        const books = data.docs.slice(0, 10); //docs is the name of the books array with the above endpoint
 
         books.forEach(book => {
             const bookCard = document.createElement("div");
             bookCard.classList.add("book");
 
-            const title = book.title || "No title available";
+        const title = book.title || "No title available";
         const author = book.author_name ? book.author_name.join(", ") : "Unknown Author";
         const coverId = book.cover_i;
         const imgSrc = coverId 
           ? `https://covers.openlibrary.org/b/id/${coverId}-M.jpg`
-          : "https://via.placeholder.com/150x220?text=No+Cover";
+          : "https://placehold.co/150x220?text=No+Cover&font=roboto";
 
         bookCard.innerHTML = `
           <img src="${imgSrc}" alt="${title}" />
@@ -54,3 +54,43 @@ function displaySearchResults(userInput) {
 }
 displaySearchResults("Harry Potter"); //default search result
 
+// display popular books from the API
+function displayPopularBooks() {
+    const popularUrl = "https://openlibrary.org/search.json?q=ud+theory";
+
+
+    fetch(popularUrl)
+    .then(response => response.json())
+    .then(data => {
+        //limit to 6 books and only those with cover image
+        const books = data.docs.filter(doc => doc.cover_i).slice(0, 6)
+        const popularContainer = document.getElementById("popular-container");
+        // console.log(popularContainer);
+        // console.log(books);
+        popularContainer.innerHTML = "";
+
+        books.forEach(book => {
+            const bookCard = document.createElement("div");
+            bookCard.classList.add("book");
+
+        const title = book.title || "No title available";
+        const author = book.author_name ? book.author_name.join(", ") : "Unknown Author";
+        const coverId = book.cover_i;
+        const imgSrc = coverId 
+          ? `https://covers.openlibrary.org/b/id/${coverId}-M.jpg`
+          : "https://placehold.co/150x220?text=No+Cover&font=roboto";
+
+        bookCard.innerHTML = `
+          <img src="${imgSrc}" alt="${title}" />
+          <h3>${title}</h3>
+          <p><em>${author}</em></p>
+          `;
+            popularContainer.appendChild(bookCard);
+        })
+
+    })
+    .catch(err => {
+      console.error("Failed to load popular books:", err);
+    });
+}
+displayPopularBooks();
